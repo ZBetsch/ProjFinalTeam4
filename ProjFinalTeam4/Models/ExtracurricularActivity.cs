bee7daf3c654617b0878f10acf019b544751851e
Ethan Goudy <tesladyne56@gmail.com>
2024-11-21 13:34:40 -0500

    added new model

---
+++ ProjFinalTeam4/Models/ExtracurricularActivity.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjFinalTeam4.Models
+{
+    public class ExtracurricularActivity
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+        public string activityDescription { get; set; }
+        public string activityLocation { get; set; }
+        public string activitySchedule { get; set; }
+        //constructors
+        public ExtracurricularActivity() { }
+        public ExtracurricularActivity(int ActivityId, string ActivityName, string ActivityDescription, string ActivityLocation, string ActivitySchedule)
+        {
+            this.activityId = ActivityId;
+            this.activityName = ActivityName;
+            this.activityDescription = ActivityDescription;
+            this.activityLocation = ActivityLocation;
+            this.activitySchedule = ActivitySchedule;
+        }
+    }
+}
