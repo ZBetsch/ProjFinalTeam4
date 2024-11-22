b7e9777f46d7becc2177e08a45a6156afbce4ada
Ethan Goudy <tesladyne56@gmail.com>
2024-11-21 21:54:50 -0500

    Second attempt at comitting

---
+++ ProjFinalTeam4/Migrations/20241122025136_TeamProjFinalData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjFinalTeam4.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeamProjFinalData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExtracurricularActivity",
+                columns: table => new
+                {
+                    activityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    activityName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    activityDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    activityLocation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    activitySchedule = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExtracurricularActivity", x => x.activityId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExtracurricularActivity");
+        }
+    }
+}
