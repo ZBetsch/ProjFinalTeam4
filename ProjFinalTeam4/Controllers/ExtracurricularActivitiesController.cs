32bb80f8b9d51e3c0c9b51701de3840568472af1
Xuan Wang <wang6xu@mail.uc.edu>
2024-11-27 02:25:37 +0800

    Add data to Travel and rewrite Travel controller code by Xuan

--- ProjFinalTeam4/Controllers/ExtracurricularActivitiesController.cs
+++ ProjFinalTeam4/Controllers/ExtracurricularActivitiesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -21,88 +22,168 @@ namespace ProjFinalTeam4.Controllers
             _context = context;
         }
 
-        // GET: api/ExtracurricularActivities
+        // GET
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ExtracurricularActivity>>> GetExtracurricularActivity()
+        public IActionResult GetTravel()
         {
-            return await _context.ExtracurricularActivity.ToListAsync();
+            return Ok(_context.Travel.ToList());
         }
 
-        // GET: api/ExtracurricularActivities/5
+        // GET(ID)
         [HttpGet("{id}")]
-        public async Task<ActionResult<ExtracurricularActivity>> GetExtracurricularActivity(int id)
+        public IActionResult GetTravel(int id)
         {
-            var extracurricularActivity = await _context.ExtracurricularActivity.FindAsync(id);
+            Travel travel = _context.Travel.Find(id);
+            if (travel == null)
+                return NotFound();
+            return Ok(travel);
+        }
 
-            if (extracurricularActivity == null)
+        // PUT
+        [HttpPut("{id}")]
+        public IActionResult PutTravel(Travel travel)
+        {
+            try
+            {
+                _context.Entry(travel).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
             {
                 return NotFound();
             }
-
-            return extracurricularActivity;
+            return Ok();
         }
 
-        // PUT: api/ExtracurricularActivities/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutExtracurricularActivity(int id, ExtracurricularActivity extracurricularActivity)
+        // POST
+        [HttpPost]
+        public IActionResult PostTravel(Travel travel)
         {
-            if (id != extracurricularActivity.activityId)
-            {
-                return BadRequest();
-            }
+            _context.Travel.Add(travel);
+            _context.SaveChanges();
+            return Ok();
+        }
 
-            _context.Entry(extracurricularActivity).State = EntityState.Modified;
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTravel(int id)
+        {
+            Travel travel = _context.Travel.Find(id);
+            if (travel == null)
+                return NotFound();
 
             try
             {
-                await _context.SaveChangesAsync();
+                _context.Travel.Remove(travel);
+                _context.SaveChanges();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ExtracurricularActivityExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/ExtracurricularActivities
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<ExtracurricularActivity>> PostExtracurricularActivity(ExtracurricularActivity extracurricularActivity)
-        {
-            _context.ExtracurricularActivity.Add(extracurricularActivity);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetExtracurricularActivity", new { id = extracurricularActivity.activityId }, extracurricularActivity);
-        }
-
-        // DELETE: api/ExtracurricularActivities/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteExtracurricularActivity(int id)
-        {
-            var extracurricularActivity = await _context.ExtracurricularActivity.FindAsync(id);
-            if (extracurricularActivity == null)
+            catch (Exception ex)
             {
                 return NotFound();
             }
+            _context.Travel.Remove(travel);
 
-            _context.ExtracurricularActivity.Remove(extracurricularActivity);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return Ok();
         }
 
-        private bool ExtracurricularActivityExists(int id)
+        [HttpPut]
+        public IActionResult PutStudent(Travel travel)
         {
-            return _context.ExtracurricularActivity.Any(e => e.activityId == id);
+            try
+            {
+                _context.Entry(travel).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+            //Below are Codes Generated Automatically:
+            //// GET: api/ExtracurricularActivities
+            //[HttpGet]
+            //public async Task<ActionResult<IEnumerable<ExtracurricularActivity>>> GetExtracurricularActivity()
+            //{
+            //    return await _context.ExtracurricularActivity.ToListAsync();
+            //}
+
+            //// GET: api/ExtracurricularActivities/5
+            //[HttpGet("{id}")]
+            //public async Task<ActionResult<ExtracurricularActivity>> GetExtracurricularActivity(int id)
+            //{
+            //    var extracurricularActivity = await _context.ExtracurricularActivity.FindAsync(id);
+
+            //    if (extracurricularActivity == null)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    return extracurricularActivity;
+            //}
+
+            //// PUT: api/ExtracurricularActivities/5
+            //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+            //[HttpPut("{id}")]
+            //public async Task<IActionResult> PutExtracurricularActivity(int id, ExtracurricularActivity extracurricularActivity)
+            //{
+            //    if (id != extracurricularActivity.activityId)
+            //    {
+            //        return BadRequest();
+            //    }
+
+            //    _context.Entry(extracurricularActivity).State = EntityState.Modified;
+
+            //    try
+            //    {
+            //        await _context.SaveChangesAsync();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!ExtracurricularActivityExists(id))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+
+            //    return NoContent();
+            //}
+
+            //// POST: api/ExtracurricularActivities
+            //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+            //[HttpPost]
+            //public async Task<ActionResult<ExtracurricularActivity>> PostExtracurricularActivity(ExtracurricularActivity extracurricularActivity)
+            //{
+            //    _context.ExtracurricularActivity.Add(extracurricularActivity);
+            //    await _context.SaveChangesAsync();
+
+            //    return CreatedAtAction("GetExtracurricularActivity", new { id = extracurricularActivity.activityId }, extracurricularActivity);
+            //}
+
+            //// DELETE
+            //[HttpDelete("{id}")]
+            //public async Task<IActionResult> DeleteExtracurricularActivity(int id)
+            //{
+            //    var extracurricularActivity = await _context.ExtracurricularActivity.FindAsync(id);
+            //    if (extracurricularActivity == null)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    _context.ExtracurricularActivity.Remove(extracurricularActivity);
+            //    await _context.SaveChangesAsync();
+
+            //    return NoContent();
+            //}
+
+            //private bool ExtracurricularActivityExists(int id)
+            //{
+            //    return _context.ExtracurricularActivity.Any(e => e.activityId == id);
+            //}
         }
     }
 }
